0fcbc35bd5c4d39a7563f25ae62fee8b5daf8bb5
Zhuoran He <zhuo1angT@Outlook.com>
2021-04-28 00:27:35 +0800

    upd bloom-filter

--- src/Bloom-Filter/BloomFilter.hh
+++ src/Bloom-Filter/BloomFilter.hh
@@ -1,14 +1,54 @@
-//
-// Created by zhuo1ang on 4/26/21.
-//
-
 #ifndef DBDS_BLOOMFILTER_HH
 #define DBDS_BLOOMFILTER_HH
 
-template<typename T>
-class BloomFilter {
+#include <vector>
+#include <functional>
+#include <bitset>
+#include <iostream>
 
+template<typename T, size_t S>
+class BloomFilter {
+private:
+
+    std::vector<std::function<size_t(T)>> hash_functions_;
+
+    std::bitset<S> bitset_;
+
+public:
+
+    BloomFilter();
+
+    BloomFilter(std::initializer_list<std::function<size_t(T)>>);
+
+    void Insert(const T &);
+
+    bool Contains(const T &) const;
 };
+
+template<typename T, size_t S>
+bool BloomFilter<T, S>::Contains(const T &value) const {
+    for (auto hash : hash_functions_)
+        if (not bitset_[hash(value) % S])
+            return false;
+    return true;
+}
+
+template<typename T, size_t S>
+BloomFilter<T, S>::BloomFilter() {
+    hash_functions_.push_back(std::hash<T>());
+}
+
+template<typename T, size_t S>
+BloomFilter<T, S>::BloomFilter(std::initializer_list<std::function<size_t(T)>> list) {
+    for (auto hash : list)
+        hash_functions_.push_back(hash);
+}
+
+template<typename T, size_t S>
+void BloomFilter<T, S>::Insert(const T &value) {
+    for (auto hash : hash_functions_)
+        bitset_.set(hash(value) % S);
+}
 
 
 #endif //DBDS_BLOOMFILTER_HH
--- src/Bloom-Filter/BloomFilterTest.cc
+++ src/Bloom-Filter/BloomFilterTest.cc
@@ -1,9 +1,17 @@
 #include <iostream>
+
 #include "BloomFilter.hh"
 
+using namespace std;
 
 int main() {
+    BloomFilter<int, 10000> filter{};
 
+    for (int i = 0; i < 10; i++)
+        filter.Insert(i);
+
+    for (int i = 0; i < 1000; i++)
+        cout << i << " " << filter.Contains(i) << endl;
 
     return 0;
 }
